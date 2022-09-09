65d26f9ce88926c7ed8d589608a228b32384cb3b
skywalker <farhangian.ahmad@gmail.com>
2022-09-09 14:27:44 +0430

    registred the model on admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogPost
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'status']
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('/', views.ListView.as_view(), name='list_view'),
+    path('', views.ListView.as_view(), name='list_view'),
 ]
