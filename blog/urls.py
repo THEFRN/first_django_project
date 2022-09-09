a79a6c93ca312db7764a696927a0c056302e78dc
skywalker <farhangian.ahmad@gmail.com>
2022-09-09 15:43:11 +0430

    added the / in the correct place

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.ListView.as_view(), name='list_view'),
-    path('/<int:pk>', views.DetailView.as_view(), name='detail_view')
+    path('<int:pk>/', views.DetailView.as_view(), name='detail_view'),
 ]
