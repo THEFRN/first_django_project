5cbd55b3a5e523588df68550d7c53ff0d7ab2516
skywalker <farhangian.ahmad@gmail.com>
2022-09-09 13:23:10 +0430

    Created a directory for templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    STATUS_CHOICES = (
+        ('pub', 'Published'),
+        ('drf', 'Draft'),
+    )
+    text = models.TextField()
+    title = models.CharField(max_length=100)
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES)
+    date_time_created = models.DateTimeField(auto_now_add=True)
+    date_time_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
