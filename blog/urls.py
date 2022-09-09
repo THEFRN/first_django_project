0d1a1764bbc2f6bc86e1c6933ef03ab80cb05e3e
skywalker <farhangian.ahmad@gmail.com>
2022-09-09 16:33:16 +0430

    created a URL pattern for delete view

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.ListView.as_view(), name='list_view'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail_view'),
     path('create/', views.CreatePost.as_view(), name='post_create'),
+    path('<int:pk>/delete', views.PostDelete.as_view(), name='delete'),
 ]
