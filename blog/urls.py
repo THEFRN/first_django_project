12556db49997131c305f58de50beec7da7330e3d
skywalker <farhangian.ahmad@gmail.com>
2022-09-09 15:51:46 +0430

    created a URL pattern for post creation

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.ListView.as_view(), name='list_view'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail_view'),
+    path('create/', views.CreatePost.as_view(), name='post_create'),
 ]
