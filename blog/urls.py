daccaff5005daf94989a56a2de2d94f4b185fe95
skywalker <farhangian.ahmad@gmail.com>
2022-09-09 16:39:19 +0430

    created a URL pattern for update view

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail_view'),
     path('create/', views.CreatePost.as_view(), name='post_create'),
     path('<int:pk>/delete', views.PostDelete.as_view(), name='delete'),
+    path('<int:pk>/update', views.PostUpdate.as_view(), name='update'),
 ]
