63d2a230ceadbd4c77a288aa8e81999df49c685e
skywalker <farhangian.ahmad@gmail.com>
2022-09-09 14:20:34 +0430

    used context in the template

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .models import BlogPost
 class ListView(generic.ListView):
     # model = BlogPost
     template_name = 'blog/list_view.html'
-    context_object_name = 'post'
+    context_object_name = 'posts'
 
     def get_queryset(self):
         return BlogPost.objects.filter(status='pub').order_by('-date_time_modified')
